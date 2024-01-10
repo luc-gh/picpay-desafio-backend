723fc140499e1f1ffdbc0ca7b57feed4e9150619
Lucas <contatolucasoficial0@gmail.com>
2024-01-09 21:57:54 -0300

    feature/user-service-methods

--- src/main/java/com/spring/picpaychallenge/services/UserService.java
+++ src/main/java/com/spring/picpaychallenge/services/UserService.java
@@ -7,20 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public void validateTransaction(User sender, BigDecimal amount){
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
         if (sender.getUserType().equals(UserType.MERCHANT)) {
-            throw new RuntimeException("Transferência inválida. Tipo de usuário não autorizado.");
+            throw new Exception("Transferência inválida. Tipo de usuário não autorizado.");
         }
         if (sender.getBalance().longValueExact() < amount.longValueExact()){
-            throw new RuntimeException("Transferência não realizada. Usuário não tem saldo para tal.");
+            throw new Exception("Transferência não realizada. Usuário não tem saldo para tal.");
         }
-        System.out.println("Transação realizada.");
+        System.out.println("Transação válida.");
     }
 
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado."));
+    }
+
+    public void saveNewUser(User user){
+        this.repository.save(user);
+    }
 }
